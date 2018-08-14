2d43e69ac7865f65f862ac5fe81b62a0fe6f62bc
Steven Shan <stevshan@microsoft.com>
2018-08-14 15:48:37 -0700

    fix gitignore.

--- build/app/modules/ipc/common.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-//-----------------------------------------------------------------------------
-// Copyright (c) 2018 Steven Shan. All rights reserved.
-// Licensed under the MIT License. See License file under the project root for license information.
-//-----------------------------------------------------------------------------
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UuidNamespace = "65ef6f94-e6c9-4c95-8360-6d29de87b1dd";
-//# sourceMappingURL=common.js.map
--- build/app/modules/package-manager/common.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-//-----------------------------------------------------------------------------
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See License file under the project root for license information.
-//-----------------------------------------------------------------------------
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PackageManagerSettingsName = "package-manager";
-//# sourceMappingURL=common.js.map
